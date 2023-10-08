f580347f6ee9a43214fcf9e23e698f6df8f2ae69
Sharbel Okzan <sharbelpib@gmail.com>
2023-10-08 16:47:30 +0200

    added auth repository

---
+++ app/lib/core/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,16 @@
+import 'package:auth_example/core/auth/data/datasources/auth_account_remote_data_source.dart';
+import 'package:injectable/injectable.dart';
+
+import '../entities/login.dart';
+import '../entities/user_auth_data.dart';
+
+@injectable
+class AuthAcountRepository {
+  final AuthAccountRemoteDataSource _accountDataSource;
+
+  AuthAcountRepository(this._accountDataSource);
+
+  Future<UserAuthData> authenticate(Login login) async {
+    return await _accountDataSource.authenticate(login);
+  }
+}
