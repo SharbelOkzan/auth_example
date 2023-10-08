d02da7506459799c59b6b1d0c6070b1c7f98421e
Sharbel Okzan <sharbelpib@gmail.com>
2023-10-08 17:13:35 +0200

    files repackaging

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:auth_example/app.dart';
-import 'package:auth_example/core/locater.dart';
+import 'package:auth_example/core/services/locater.dart';
 import 'package:flutter/material.dart';
 
 void main() {
