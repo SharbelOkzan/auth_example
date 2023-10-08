12e2dc238cd1d191b986e88f65fa772e51ca2cea
Sharbel Okzan <sharbelpib@gmail.com>
2023-10-08 17:41:37 +0200

    added secure storage service

--- app/lib/core/services/locater.config.dart
+++ app/lib/core/services/locater.config.dart
@@ -10,14 +10,21 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auth_example/client/api_client_manager.dart' as _i3;
-import 'package:auth_example/core/auth/domain/repositories/auth_repository.dart'
-    as _i5;
-import 'package:auth_example/core/auth/domain/usecases/login_via_password_usecase.dart'
+import 'package:auth_example/core/auth/data/datasources/auth_account_remote_data_source.dart'
+    as _i8;
+import 'package:auth_example/core/auth/data/mappers/login_mapper.dart' as _i5;
+import 'package:auth_example/core/auth/data/mappers/user_auth_info_mapper.dart'
     as _i4;
+import 'package:auth_example/core/auth/domain/repositories/auth_repository.dart'
+    as _i10;
+import 'package:auth_example/core/auth/domain/usecases/login_via_password_usecase.dart'
+    as _i11;
 import 'package:auth_example/core/auth/domain/usecases/presist_token_usecase.dart'
     as _i6;
 import 'package:auth_example/core/auth/presentation/bloc/authentication_bloc/authentication_bloc.dart'
-    as _i7;
+    as _i12;
+import 'package:auth_example/core/services/secure_storage_service.dart' as _i7;
+import 'package:authexampleclient/authexampleclient.dart' as _i9;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -33,12 +40,23 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     gh.factory<_i3.ApiClientManager>(() => _i3.ApiClientManager());
-    gh.factory<_i4.GetCredentialsViaPasswordUsecase>(() =>
-        _i4.GetCredentialsViaPasswordUsecase(gh<_i5.AuthAcountRepository>()));
+    gh.factory<_i4.AuthUserMapper>(() => _i4.AuthUserMapper());
+    gh.factory<_i5.LoginMapper>(() => _i5.LoginMapper());
     gh.factory<_i6.PresistTokenUsecase>(() => _i6.PresistTokenUsecase());
-    gh.factory<_i7.AuthenticationBloc>(() => _i7.AuthenticationBloc(
+    gh.factory<_i7.SecureStorageService>(() => _i7.SecureStorageService());
+    gh.factory<_i8.AuthAccountRemoteDataSource>(
+        () => _i8.AuthAccountRemoteDataSource(
+              authAccountApi: gh<_i9.AuthAccountApi>(),
+              loginMapper: gh<_i5.LoginMapper>(),
+              authUserMapper: gh<_i4.AuthUserMapper>(),
+            ));
+    gh.factory<_i10.AuthAcountRepository>(
+        () => _i10.AuthAcountRepository(gh<_i8.AuthAccountRemoteDataSource>()));
+    gh.factory<_i11.GetCredentialsViaPasswordUsecase>(() =>
+        _i11.GetCredentialsViaPasswordUsecase(gh<_i10.AuthAcountRepository>()));
+    gh.factory<_i12.AuthenticationBloc>(() => _i12.AuthenticationBloc(
           gh<_i3.ApiClientManager>(),
-          gh<_i4.GetCredentialsViaPasswordUsecase>(),
+          gh<_i11.GetCredentialsViaPasswordUsecase>(),
           gh<_i6.PresistTokenUsecase>(),
         ));
     return this;
---
+++ app/lib/core/services/secure_storage_service.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class SecureStorageService {
+  FlutterSecureStorage? _flutterStorage;
+
+  FlutterSecureStorage get _storage =>
+      _flutterStorage ??= const FlutterSecureStorage();
+
+  Future<String?> getString(String key) async {
+    String? value = await _storage.read(key: key);
+    return value;
+  }
+
+  Future<void> persistString(String key, String value) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  Future<void> clearRecord(String key) async {
+    await _storage.delete(key: key);
+  }
+}
